a60c9cebe7c90facfcbf7c04d07df432da0bf7b5
ABOUADANE El Mehdi <hidou-abou@live.fr>
2020-01-16 18:29:40 +0100

    suite billing

--- billing-service/src/main/java/org/sid/billingservice/BillingServiceApplication.java
+++ billing-service/src/main/java/org/sid/billingservice/BillingServiceApplication.java
@@ -1,8 +1,10 @@
 package org.sid.billingservice;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,6 +17,7 @@ import org.springframework.data.rest.core.config.Projection;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.*;
 import java.util.Collection;
@@ -26,6 +29,7 @@ class Bill {
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private Date billingDate;
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
 	private Long customerID;
 	@Transient
 	private Customer customer;
@@ -51,12 +55,14 @@ interface BillProjection {
 class ProductItem {
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
 	private Long productID;
 	@Transient
 	private Product product;
 	private double price;
 	private double quantite;
 	@ManyToOne
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
 	private Bill bill;
 }
 
@@ -110,13 +116,35 @@ public class BillingServiceApplication {
 			System.out.println("Name = "+c1.getName());
 			System.out.println("Email = "+c1.getEmail());
 			System.out.println("***********************");
-			Bill bill1 = billRepository.save(new Bill(null, new Date(), c1.getId(), null));
+			Bill bill1 = billRepository.save(new Bill(null, new Date(), c1.getId(), null, null));
 
 			PagedModel<Product> products = inventoryService.findAllProducts();
 			products.getContent().forEach(p -> {
-				productItemRepository.save(new ProductItem(null, p.getId(), p.getPrice(), 30, bill1));
+				productItemRepository.save(new ProductItem(null, p.getId(), null, p.getPrice(), 30, bill1));
 			});
 		};
 	}
 
+}
+
+@RestController
+class BillRestController {
+	@Autowired
+	private BillRepository billRepository;
+	@Autowired
+	private ProductItemRepository productItemRepository;
+	@Autowired
+	private CustomerService customerService;
+	@Autowired
+	private InventoryService inventoryService;
+
+	@GetMapping("/fullBill/{id}")
+	public Bill getBill(@PathVariable(name = "id") Long id) {
+		Bill bill = billRepository.findById(id).get();
+		bill.setCustomer(customerService.findCustomerById(bill.getCustomerID()));
+		bill.getProductItems().forEach(prod -> {
+			prod.setProduct(inventoryService.findProductById(prod.getProductID()));
+		});
+		return bill;
+	}
 }
